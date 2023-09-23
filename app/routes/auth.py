5a185b4f5744e6e5316c462672768264fd734c9f
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2023-09-22 22:54:46 -0500

    ya tenemos los servicios de registro y autenticacion

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -4,10 +4,15 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import text  # Importa la función text de SQLAlchemy
 from flask import render_template
 from app.models.user import User  # Importa la clase User del modelo
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import check_password_hash
 
 
 # Crea un Blueprint para las rutas de autenticación
 auth_bp = Blueprint('auth', __name__)
+
+# Definición de rutas y vistas
+
 
 # usando temaplate
 @auth_bp.route('/')
@@ -16,7 +21,7 @@ def index():
     return render_template('index.html', mensaje=mensaje)
 
 
-# Definición de rutas y vistas
+# Este servicio sirve para validar la conexion con la bd
 @auth_bp.route('/test_db_connection', methods=['GET'])
 def test_db_connection():
     try:
@@ -31,12 +36,12 @@ def test_db_connection():
         return 'Error de conexión a la base de datos: ' + str(e), 500
 
 
-
+# Vista Register
 @auth_bp.route('/register',  methods=['GET'])
 def formRegister():
     return render_template('register.html')
 
-
+# Servicio post register
 @auth_bp.route('/register',  methods=['POST'])
 def register():
 
@@ -59,12 +64,40 @@ def register():
         db.session.add(nuevo_usuario)
         db.session.commit()
  
-    return jsonify({'data': true}), 200
+    return jsonify({'message': 'Nuevo usuario registrado.'}), 200
 
 
+# Vista del formulario Login
+@auth_bp.route('/login',  methods=['GET'])
+def formLogin():
+    return render_template('login.html')
+
+
+# Servicio post login, si la contrasena es correcta retorna un token que debe guardarse en las cookies
 @auth_bp.route('/login', methods=['POST'])
 def login():
-    # Lógica para la autenticación de usuarios
-    # ...
-    return jsonify({'access_token': access_token}), 200
+    data = request.get_json()
+    nick = data.get('nick')
+    password = data.get('password')
 
+
+    print(nick + " " + password)
+    # Busca al usuario por su nick en la base de datos
+    user = User.query.filter_by(nick=nick).first()
+    if not user:
+        return jsonify({'message': 'Usuario no encontrado'}), 404
+
+    print( " ----------------------" )
+    print( user.password )
+    print( password )
+    print( " ----------------------" )
+    
+    # check_password_hash se utiliza para verificar contraseñas en texto claro con su io
+    ## if not check_password_hash(user.password, password):
+    if user.password == password :
+        return jsonify({'message': 'Contraseña incorrecta'}), 401
+
+    # Genera un token JWT para el usuario
+    access_token = create_access_token(identity=user.id)
+
+    return jsonify({'access_token': access_token}), 200
